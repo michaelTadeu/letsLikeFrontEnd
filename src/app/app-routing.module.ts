2c1c0ed0f307dc8cdf7f84cbb019026945472fb9
michaelTadeu <michael89.oliveira@gmail.com>
2021-11-26 18:38:11 -0300

    Ajustes nas pastas do projeto.

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,9 +1,6 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
-
 import { HomeComponent } from './home';
-import { RoomsComponent } from './rooms/rooms.component';
-import { PageComponent } from './page/page.component';
 import { AuthGuard } from './_helpers';
 
 const accountModule = () => import('./account/account.module').then(x => x.AccountModule);
@@ -12,14 +9,10 @@ const usersModule = () => import('./users/users.module').then(x => x.UsersModule
 
 const routes: Routes = [
     { path: '', component: HomeComponent, canActivate: [AuthGuard] },
-   { path: 'users', loadChildren: usersModule, canActivate: [AuthGuard] },
+    { path: 'users', loadChildren: usersModule, canActivate: [AuthGuard] },
     { path: 'account', loadChildren: accountModule },
-    {path: 'rooms', component: RoomsComponent, canActivate: [AuthGuard] },
-    {path: 'room1', component: PageComponent, canActivate: [AuthGuard], data: {page: 'room1' } },
-    {path: 'room2', component: PageComponent, canActivate: [AuthGuard], data: {page: 'room2' }},
-    {path: 'room3', component: PageComponent, canActivate: [AuthGuard], data: {page: 'room3' }},
 
-    // otherwise redirect to home
+    // Redireciona para home
     { path: '**', redirectTo: '' }
 ];
 
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,32 +2,21 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { ReactiveFormsModule } from '@angular/forms';
 import { HttpClientModule, HTTP_INTERCEPTORS } from '@angular/common/http';
-
-
 import { MatButtonModule } from '@angular/material/button';
 import { MatCardModule } from '@angular/material/card';
 import { MatToolbarModule } from '@angular/material/toolbar';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 import { FormsModule } from '@angular/forms';
-
-
-// used to create fake backend
-import { fakeBackendProvider } from './_helpers';
-
 import { AppRoutingModule } from './app-routing.module';
 import { JwtInterceptor, ErrorInterceptor } from './_helpers';
 import { AppComponent } from './app.component';
 import { AlertComponent } from './_components';
 import { HomeComponent } from './home';
-import {MyMaterialModule} from './material';
+import { MyMaterialModule } from './material';
 import { ChartsModule } from 'ng2-charts';
-import { BarChartComponent } from './charts/bar-chart/bar-chart.component';
-import { LineChartComponent } from './charts/line-chart/line-chart.component';
-import { PieChartComponent } from './charts/pie-chart/pie-chart.component';
 import { PageComponent } from './page/page.component';
 import { ContentService } from './shared/services/content.service';
 import { FullpageDirective } from './shared/directives/fullpage.directive';
-import { RoomsComponent } from './rooms/rooms.component';
 
 @NgModule({
     imports: [
@@ -47,10 +36,6 @@ import { RoomsComponent } from './rooms/rooms.component';
         AppComponent,
         AlertComponent,
         HomeComponent,
-        RoomsComponent,
-        BarChartComponent,
-        LineChartComponent,
-        PieChartComponent,
         PageComponent,
         FullpageDirective
     ],
@@ -58,7 +43,6 @@ import { RoomsComponent } from './rooms/rooms.component';
         { provide: HTTP_INTERCEPTORS, useClass: JwtInterceptor, multi: true },
         { provide: HTTP_INTERCEPTORS, useClass: ErrorInterceptor, multi: true },
 
-        // provider used to create fake backend
         ContentService
     ],
     bootstrap: [AppComponent]
