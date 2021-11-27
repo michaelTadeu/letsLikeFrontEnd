5cbae00a06f451cb2fed490a93ae5ba24b425a5c
michaelTadeu <michael89.oliveira@gmail.com>
2021-11-26 22:20:29 -0300

    Implementação do Cadastro de Usuário.

--- src/app/account/register.component.ts
+++ src/app/account/register.component.ts
@@ -7,10 +7,32 @@ import { AccountService, AlertService } from '@app/_services';
 
 @Component({ templateUrl: 'register.component.html' })
 export class RegisterComponent implements OnInit {
+    form: FormGroup;
+    submitted = false;
+    loading = false;
+
     constructor(
+        private formBuilder: FormBuilder,
+        private route: ActivatedRoute,
+        private router: Router,
+        private accountService: AccountService,
+        private alertService: AlertService
     ) { }
 
     ngOnInit() {
-       
+       this.form = this.formBuilder.group ({
+           nome: ['', Validators.required]
+       })
+    }
+
+    get f() { 
+        return this.form.controls;
+    }
+
+    onSubmit() {
+        // Verificar se todos campos obrigatórios foram preenchidos
+        
+        //Service para mandar as informações para o back-end
+        //Retorna para nossa tela de Login
     }
 }
